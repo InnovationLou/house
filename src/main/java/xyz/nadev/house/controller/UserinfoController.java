c17f64bb56157f358b96f75b9f1c6618676ec3aa
NadevXiang <479803313@qq.com>
2019-12-12 23:22:35 +0800

    add redis

---
+++ src/main/java/xyz/nadev/house/controller/UserinfoController.java
@@ -0,0 +1,10 @@
+package xyz.nadev.house.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserinfoController {
+
+}
