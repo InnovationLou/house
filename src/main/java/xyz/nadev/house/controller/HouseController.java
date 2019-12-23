b96d0eb909ad72ded6ab44dc3ffca2375e36ff7d
innovation <578777205@qq.com>
2019-12-23 14:47:24 +0800

    url地址修改

--- src/main/java/xyz/nadev/house/controller/HouseController.java
+++ src/main/java/xyz/nadev/house/controller/HouseController.java
@@ -29,13 +29,13 @@ public class HouseController {
     }
 
     @ApiOperation("新增房源信息")
-    @PostMapping("")
+    @PutMapping("")
     public ResponseVO AddHouse(House house) {
         return houseService.addHouse(house);
     }
 
     @ApiOperation("修改房源信息")
-    @PutMapping("")
+    @PostMapping("")
     public ResponseVO modifyHouse(House house) {
         return houseService.modifyHouse(house);
     }
