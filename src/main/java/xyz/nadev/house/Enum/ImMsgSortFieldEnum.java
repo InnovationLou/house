6e39e0465f0502ad3bd4a28e597efdcd7af5a54a
mayberk <qkmc@outlook.com>
2019-12-31 19:51:17 +0800

    #I17MTP tim历史消息排序有问题,时而无效

---
+++ src/main/java/xyz/nadev/house/Enum/ImMsgSortFieldEnum.java
@@ -0,0 +1,15 @@
+package xyz.nadev.house.Enum;
+
+/**
+ * msg通过哪个字段来排序
+ */
+public enum ImMsgSortFieldEnum {
+    /**
+     *
+     * 通过ID排序
+     * 通过读取消息时间排序
+     * 通过发送时间排序
+     */
+    ID,GMT_READ,GMT_SEND
+
+}
--- src/main/java/xyz/nadev/house/comparator/ImMsgComparator.java
+++ src/main/java/xyz/nadev/house/comparator/ImMsgComparator.java
@@ -1,5 +1,6 @@
 package xyz.nadev.house.comparator;
 
+import xyz.nadev.house.Enum.ImMsgSortFieldEnum;
 import xyz.nadev.house.entity.ImMsg;
 
 import java.util.Comparator;
@@ -10,22 +11,56 @@ import java.util.Comparator;
  */
 public class ImMsgComparator implements Comparator<ImMsg> {
 
+    private final static Integer LESS = -1;
+    private final static Integer EQUAL = 0;
+    private final static Integer BIG = 1;
+    /**
+     * 排序
+     * ID 或者 时间
+     * 默认通过ID排序
+     */
+    private static ImMsgSortFieldEnum type = ImMsgSortFieldEnum.ID;
 
     @Override
     public int compare(ImMsg o1, ImMsg o2) {
-        long o1t = o1.getGmtSend().getTime();
-        long o2t = o2.getGmtSend().getTime();
-        if (o1t < o2t){
-            return -1;
-        }else if(o1t == o2t){
-            return 0;   //impossible
-        }else {
-            return 0;
+        if (type == ImMsgSortFieldEnum.ID){
+            if (o1.getId() < o2.getId()){
+                return this.LESS;
+            }else if(o1.getId() == o2.getId()){
+                return this.EQUAL;   //impossible
+            }else {
+                return this.BIG;
+            }
+        } else if (type == ImMsgSortFieldEnum.GMT_READ || type == ImMsgSortFieldEnum.GMT_READ){
+            long o1t = (type == ImMsgSortFieldEnum.GMT_READ)?o1.getGmtRead().getTime():o1.getGmtSend().getTime();
+            long o2t = (type == ImMsgSortFieldEnum.GMT_READ)?o2.getGmtRead().getTime():o2.getGmtSend().getTime();
+            if (o1t < o2t){
+                return LESS;
+            }else if(o1t == o2t){
+                return EQUAL;   //impossible
+            }else {
+                return BIG;
+            }
+        } else {
+            // 默认按照ID排序
+            if (o1.getId() < o2.getId()){
+                return this.LESS;
+            }else if(o1.getId() == o2.getId()){
+                return this.EQUAL;   //impossible
+            }else {
+                return this.BIG;
+            }
         }
     }
-
     @Override
     public boolean equals(Object obj) {
         return false;
+    }
+    public static ImMsgSortFieldEnum getType() {
+        return type;
+    }
+
+    public static void setType(ImMsgSortFieldEnum type) {
+        ImMsgComparator.type = type;
     }
 }
